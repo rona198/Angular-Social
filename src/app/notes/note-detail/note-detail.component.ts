97333b548500718cd4b27fd6b77de8bc1db252fe
rona198 <laven255@gmail.com>
2018-03-13 05:38:45 -0700

    change firestore into timeline

--- src/app/notes/note-detail/note-detail.component.ts
+++ src/app/notes/note-detail/note-detail.component.ts
@@ -24,6 +24,14 @@ export class NoteDetailComponent {
     }
   }
 
+  minusHeartToNote(val: number) {
+    if (this.note.id) {
+      this.noteService.updateNote(this.note.id, { hearts: val - 1 });
+    } else {
+      console.error('Note missing ID!');
+    }
+  }
+
   deleteNote(id: string) {
     this.noteService.deleteNote(id);
   }
